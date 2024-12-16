e9497bb718cd36e3c6db24778544d34cafbf6b12
Maydan Wienreb <maydanw@gmail.com>
2024-12-16 13:32:41 +0200

    Update setup script to change storage directories and default graph spec

--- set_up_test_env.sh
+++ set_up_test_env.sh
@@ -3,21 +3,21 @@
 #These environment variables are required by Data Services. See the README for more information.
 
 #ORION_STORAGE - a directory for storing data sources
-mkdir -p "$PWD/../ORION_storage"
-export ORION_STORAGE="$PWD/../ORION_storage/"
+mkdir -p "$PWD/Data/ORION_storage"
+export ORION_STORAGE="$PWD/Data/ORION_storage/"
 
 #ORION_GRAPHS - a directory for storing knowledge graphs
-mkdir -p "$PWD/../ORION_graphs"
-export ORION_GRAPHS="$PWD/../ORION_graphs/"
+mkdir -p "$PWD/Data/ORION_graphs"
+export ORION_GRAPHS="$PWD/Data/ORION_graphs/"
 
 #ORION_LOGS - a directory for storing logs
-mkdir -p "$PWD/../ORION_logs"
-export ORION_LOGS="$PWD/../ORION_logs/"
+mkdir -p "$PWD/Data/ORION_logs"
+export ORION_LOGS="$PWD/Data/ORION_logs/"
 
 #Use EITHER of the following, ORION_GRAPH_SPEC or ORION_GRAPH_SPEC_URL
 
 #ORION_GRAPH_SPEC - the name of a Graph Spec file located in the graph_specs directory of ORION
-export ORION_GRAPH_SPEC=testing-graph-spec.yml
+export ORION_GRAPH_SPEC=default-graph-spec.yml
 
 #ORION_GRAPH_SPEC_URL - a URL pointing to a Graph Spec file
 #export ORION_GRAPH_SPEC_URL=https://raw.githubusercontent.com/RENCI-AUTOMAT/ORION/helm_deploy/graph_specs/yeast-graph-spec.yml
@@ -26,9 +26,9 @@ export PYTHONPATH="$PYTHONPATH:$PWD"
 
 
 #The following environment variables are optional
-# export EDGE_NORMALIZATION_ENDPOINT=https://bl-lookup-sri.renci.org/
-# export NODE_NORMALIZATION_ENDPOINT=https://nodenormalization-sri.renci.org/
-# export NAME_RESOLVER_ENDPOINT=https://name-resolution-sri.renci.org/
+export EDGE_NORMALIZATION_ENDPOINT=https://bl-lookup-sri.renci.org/
+export NODE_NORMALIZATION_ENDPOINT=https://nodenormalization-sri.renci.org/
+export NAME_RESOLVER_ENDPOINT=https://name-resolution-sri.renci.org/
 # export ORION_OUTPUT_URL=https://localhost/  # this is currently only used to generate metadata
 # export BL_VERSION=4.2.1
 
